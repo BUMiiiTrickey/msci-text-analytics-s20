9acc6cadb2a5e9c27add4d3c844c3123ed55c162
SHUO LIU <65793583+BUMiiiTrickey@users.noreply.github.com>
2020-06-20 21:38:38 -0400

    Create inference.py

---
+++ Assignment 3/inference.py
@@ -0,0 +1,15 @@
+from gensim.models import Word2Vec
+
+def main():
+
+    w2v =Word2Vec.load('Assignment3/data/w2v.model')
+    print('Most similar words to GOOD')
+    print(w2v.wv.most_similar('good',topn=20))
+    print('Most similar words to BAD')
+    print(w2v.wv.most_similar('bad',topn=20))
+
+
+
+
+if __name__ == '__main__':
+    print(main())

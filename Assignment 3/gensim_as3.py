002a637702a91e75d95b685dae8252e62a6fc8cc
SHUO LIU <65793583+BUMiiiTrickey@users.noreply.github.com>
2020-06-20 21:40:55 -0400

    Create gensim_as3.py

---
+++ Assignment 3/gensim_as3.py
@@ -0,0 +1,22 @@
+import sys
+from gensim.models import Word2Vec
+from out import read_dataset
+
+
+
+def main(data_path):
+    all_lines=read_dataset(data_path)
+    print('Splitting lines in the dataset')
+    all_lines = [line.strip().split() for line in all_lines]
+    print('Training word2vec model')
+
+    w2v = Word2Vec(all_lines, size=100, window=5, min_count=1, workers=4)
+    w2v.save('Assignment3/data/w2v.model')
+
+    w2v1=Word2Vec.load('Assignment3/data/w2v.model')
+    print(w2v1.wv.most_similar('good',topn=20))
+    print(w2v1.wv.most_similar('bad',topn=20))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])

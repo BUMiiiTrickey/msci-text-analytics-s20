83105a2920119bc84de80a4b12bf5e414b98bffd
SHUO LIU <65793583+BUMiiiTrickey@users.noreply.github.com>
2020-06-22 17:06:18 -0400

    Update out.py

--- Assignment 3/out.py
+++ Assignment 3/out.py
@@ -3,11 +3,9 @@ import random
 
 
 def read_dataset(data_path):
-    with open("/20863354_s647liu/Assignment3/data/pos.txt") as f:
-        pos_lines = f.readlines()
-    with open("/20863354_s647liu/Assignment3/data/neg.txt") as f:
-        neg_lines = f.readlines()
-    return(pos_lines + neg_lines)
+    with open("20863354_s647liu/as3/data/out.txt") as f:
+        out_lines = f.readlines()
+    return(out_lines)
 
 
 def main(data_path):
@@ -25,6 +23,37 @@ def main(data_path):
     val_size = int(0.1 * total_lines)
 
     random.shuffle(all_lines)
+
+    for idx, line in enumerate(all_lines):
+        sentence = line[0].strip().split()
+        label = line[1]
+
+        no_punctuation_documents = []
+        for i in sentence:
+            no_punctuation_documents.append(i.translate(str.maketrans('', '', '!"#$%&()*+/:;<=>@[\\]^`{|}~\t\n')))
+
+        csv_line = '{}\n'.format(','.join(no_punctuation_documents))
+        csv_data += csv_line
+        labels_data += '{}\n'.format(label)
+
+        if idx < train_size:
+            train_data += csv_line
+        elif idx >= train_size and idx < train_size + val_size:
+            val_data += csv_line
+        else:
+            test_data += csv_line
+
+        for word in sentence:
+            word = word.lower()
+            if word not in vocab:
+                vocab[word] = 0
+            vocab[word] += 1
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
 
     for idx, line in enumerate(all_lines):
         sentence = line[0].strip().split()

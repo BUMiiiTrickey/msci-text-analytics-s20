301da2c2dfc8600246bdff493dd78bcf1bcaedc8
SHUO LIU <65793583+BUMiiiTrickey@users.noreply.github.com>
2020-06-22 17:07:20 -0400

    Update inference.py

--- Assignment 3/inference.py
+++ Assignment 3/inference.py
@@ -1,15 +1,15 @@
+import sys
 from gensim.models import Word2Vec
 
-def main():
 
-    w2v =Word2Vec.load('/20863354_s647liu/Assignment3/data/w2v.model')
-    print('Most similar words to GOOD')
-    print(w2v.wv.most_similar('good',topn=20))
-    print('Most similar words to BAD')
-    print(w2v.wv.most_similar('bad',topn=20))
-
-
+def main(data_path):
+    with open('20863354_s647liu/as3/data/sample_words.txt') as f:
+        sample_text = f.readlines()
+    sample_text = [w.strip() for w in sample_text]
+    w2v = Word2Vec.load('20863354_s647liu/as3/data/w2v.model')
+    return ['{} => {}'.format(w, [o[0] for o in w2v.most_similar([w2v[w]], topn=20)[1:]]) for w in sample_text]
 
 
 if __name__ == '__main__':
-    print(main())
+    words = main(sys.argv[1])
+    print('\n\n'.join(words))

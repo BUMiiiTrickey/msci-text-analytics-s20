b863d88f86446d4686a2f9ce2a061a0ba814e7ce
SHUO LIU <65793583+BUMiiiTrickey@users.noreply.github.com>
2020-06-22 17:06:48 -0400

    Update gensim_as3.py

--- Assignment 3/gensim_as3.py
+++ Assignment 3/gensim_as3.py
@@ -11,11 +11,11 @@ def main(data_path):
     print('Training word2vec model')
 
     w2v = Word2Vec(all_lines, size=100, window=5, min_count=1, workers=4)
-    w2v.save('Assignment3/data/w2v.model')
+    w2v.save('20863354_s647liu/as3/data/w2v.model')
 
-    w2v1=Word2Vec.load('/20863354_s647liu/Assignment3/data/w2v.model')
-    print(w2v1.wv.most_similar('good',topn=20))
-    print(w2v1.wv.most_similar('bad',topn=20))
+    #w2v1=Word2Vec.load('/Users/iris/Desktop/20863354_s647liu/Assignment3/data/w2v.model')
+    #print(w2v1.wv.most_similar('good',topn=20))
+    #print(w2v1.wv.most_similar('bad',topn=20))
 
 
 if __name__ == '__main__':
